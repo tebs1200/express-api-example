9ae8d6add866144d87120e2e900760966c162d68
Aaren Tebbutt <aaren.tebbutt@gmail.com>
2016-02-02 02:17:05 +1100

    Added a basic app.js file that reads in the config and sets up the logger

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    logger = require('log4js').getLogger(),
+    config = require('./config/index.js');
+
+var app = express();
+logger.setLevel(config.get('logLevel'));
+
+app.locals.config = config;
+app.locals.logger = logger;
+
+app.use(function(req, res) {
+    logger.warn('Request for \'' + req.url + '\' resulted in 404.');
+    res.status(404).send('Sorry cant find that!');
+});
+
+app.listen(config.get('port'), function() {
+    logger.info('Example app listening on port 3000!');
+});
