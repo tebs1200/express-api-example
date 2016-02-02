0a7b5472f3d3eaed44064264b5d5634418388305
Aaren Tebbutt <aaren.tebbutt@gmail.com>
2016-02-03 00:19:30 +1100

    Added additional test examples with chai 'should' syntax

--- test/bdd/index.js
+++ test/bdd/index.js
@@ -1,12 +1,32 @@
 var api = require('../../server.js'),
-    request = require('supertest')(api);
+    request = require('supertest')(api),
+    should = require('chai').should();
 
 describe('Widgets', function() {
 
-    it('Return a 200 response', function(done) {
+    it('returns a 200 response', function(done) {
         request
             .get('/widgets')
             .expect(200, done);
+    });
+
+    it('returns json', function(done) {
+        request
+            .get('/widgets')
+            .set('Accept', 'application/jsom')
+            .expect('Content-Type', 'application/json', done);
+    });
+
+    it('should have "hello" property with value "world"', function(done) {
+        request
+            .get('/widgets')
+            .set('Accept', 'application/jsom')
+            .expect(200)
+            .end(function(err, res) {
+                res.body.should.have.property("hello");
+                res.body.hello.should.equal("world");
+                done();
+            });
     });
 
 });
